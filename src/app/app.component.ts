85d22b1903eb8604d1a83ff600e394ca281f50e4
KStepczyńska <stepczynska.katarzyna@gmail.com>
2024-10-09 21:57:19 +0200

    finish challenge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { Observable } from 'rxjs';
 import { RouterOutlet } from '@angular/router';
+
+export interface CardModel {
+  fact: string;
+}
 
 @Component({
   selector: 'app-root',
+  template: `
+    <div class="mw-100 vh-100 d-flex justify-content-center align-items-center bg-light">
+      <div class="card text-center">
+        <div class="card-body">
+          <p class="p-2 m-0 fs-6 fw-semibold text-secondary">{{ (data$ | async)?.fact }}</p>
+        </div>
+      </div>
+    </div>
+  `,
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RouterOutlet, HttpClientModule, AsyncPipe],
+  // templateUrl: './app.component.html',
+  // styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'mentoring-program-starter-kit';
+  httpClient: HttpClient = inject(HttpClient);
+  data$: Observable<CardModel> = this.httpClient.get<CardModel>(
+    'https://catfact.ninja/fact'
+  );
 }
